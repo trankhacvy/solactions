e1c1816d3b91bdfcd70bbfb07d8a7bc9d5c216c5
Kay (Andrew) <chauanhtuan185@gmail.com>
2024-08-02 22:25:23 +0700

    feat: add show link for talkwithme (UI)

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -2,3 +2,6 @@ import { env } from "@/env";
 
 export const getDonationLink = (slug: string) =>
   `${env.NEXT_PUBLIC_FE_BASE_URL}/${slug}`;
+
+export const getTalkwithmeLink = (slug: string) => 
+  `${env.NEXT_PUBLIC_FE_BASE_URL}/api/talkwithme/${slug}`;
