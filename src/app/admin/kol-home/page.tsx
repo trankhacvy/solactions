03a87c084e0b06ad9f1e9e5edf69deeb923709ec
Kay (Andrew) <chauanhtuan185@gmail.com>
2024-10-06 23:46:26 +0700

    feat: update

---
+++ src/app/admin/kol-home/page.tsx
@@ -0,0 +1,37 @@
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+import { notFound } from "next/navigation";
+import { Box, Stack, Typography } from "@mui/material";
+import { YourLinkKol } from "@/components/admin/your-link-kol";
+// import { MoreMeetings } from "@/components/admin/more-meetings";
+// import { PreviewKolCard } from "@/components/admin/preview-kol-card";
+
+export default async function AdminPage() {
+  const session = await getServerAuthSession();
+  const profile = await api.donation.me();
+  if (!profile) {
+    return notFound();
+  }
+  return (
+    <Stack width="100%" flexDirection={{ xs: "column", md: "row" }} flex={1}>
+      <Stack
+        flex={1}
+        px={{ xs: 2, md: 3, lg: 5 }}
+        py={{
+          xs: 3,
+          md: 5,
+        }}
+        gap={6}
+      >
+        <Box>
+          <Typography mb={2} variant="h4">
+            Call with me, {session?.user.name} 👋
+          </Typography>
+          <YourLinkKol profile={profile}/>
+        </Box>
+        {/* <MoreMeetings /> */}
+      </Stack>
+      {/* <PreviewKolCard profile={profile} /> */}
+    </Stack>
+  );
+}
